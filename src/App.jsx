a26a253f0a4feb9783c70c2f82cf520f92c2a045
UnesABA <unesaitbenali@gmail.com>
2025-07-14 13:09:40 +0100

    Add basic footer with logo and social links

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'remixicon/fonts/remixicon.css'
 import { BrowserRouter } from "react-router-dom"
 import Layout from "./component/layout/Layout"
 import "./index.css"
--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import logo from "../../assets/images/logo.jpg"; 
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-content">
+        <img src={logo} alt="logo" className="footer-logo" />
 
-export default Footer
+        <div className="social-icons">
+          <a href="https://github.com/UnesABA" target="_blank" rel="noopener noreferrer">
+            <FaGithub />
+          </a>
+          <a href="https://www.linkedin.com/in/younes-ait-ben-ali-506126361/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin />
+          </a>
+        </div>
+
+        <p>&copy; {new Date().getFullYear()} FitnessFirst. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
