6af07d74164fb512305b44129a8063e73164d879
UnesABA <unesaitbenali@gmail.com>
2025-07-19 15:48:33 +0100

    Fix layout to keep footer at the bottom of all pages

--- src/component/layout/Layout.jsx
+++ src/component/layout/Layout.jsx
@@ -1,15 +1,17 @@
-import Header from "../header/Header.jsx"
-import Routes from "../../routes/Routes.jsx"
-import Footer from "../footer/Footer.jsx"
+import Header from "../header/Header.jsx";
+import Routes from "../../routes/Routes.jsx";
+import Footer from "../footer/Footer.jsx";
 
 const Layout = () => {
   return (
-    <>
+    <div className="layout">
       <Header />
-      <Routes />
+      <main>
+        <Routes />
+      </main>
       <Footer />
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
